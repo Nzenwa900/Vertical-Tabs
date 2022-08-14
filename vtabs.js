429658534fd01cd7b458589b82943881b2a0e516
Nzenwa Chidozie <cyrilnze@gmail.com>
2022-08-14 04:52:32 +0100

    initiated changes

---
+++ vtabs.js
@@ -0,0 +1,13 @@
+
+const tabLinks = document.getElementsByClassName(".tablink");
+const allContent = document.querySelectorAll(".tab-content");
+
+for(let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function(e){
+        const current = document.getElementsByClassName("active");
+
+        current[0].className = current[0].className.replace(" active", "");
+
+        this.className += " active";
+    })
+}
